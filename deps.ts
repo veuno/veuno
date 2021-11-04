51348e008b777bcf6d6bbb9099957f270dcdadc5
Nuno Garcia <nuno.garcia@wtvision.com>
2021-11-04 16:58:13 +0000

    removing old setup

--- deps.ts
+++
@@ -1,6 +0,0 @@
-// Standard Library
-
-// Third Party Modules
-export { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
-export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.28.0/mod.ts";
-export { connect } from "https://deno.land/x/cotton@v0.7.5/mod.ts";
--- sql/create table migrations.sql
+++
@@ -1,9 +0,0 @@
-
-create table veuno.migrations
-(
-    id int auto_increment,
-    name varchar(255) null,
-    batch int null,
-    constraint migrations_pk
-        primary key (id)
-);
